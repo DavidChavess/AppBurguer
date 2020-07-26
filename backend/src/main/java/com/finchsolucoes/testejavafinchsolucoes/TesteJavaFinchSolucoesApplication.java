c2b602261ab826e1683e570fbdd3a45b496457bf
David Chaves <davi.ch.fe@gmail.com>
2020-07-26 15:43:23 -0300

    Upload backend

---
+++ backend/src/main/java/com/finchsolucoes/testejavafinchsolucoes/TesteJavaFinchSolucoesApplication.java
@@ -0,0 +1,13 @@
+package com.finchsolucoes.testejavafinchsolucoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteJavaFinchSolucoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteJavaFinchSolucoesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/finchsolucoes/testejavafinchsolucoes/TesteJavaFinchSolucoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finchsolucoes.testejavafinchsolucoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteJavaFinchSolucoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
